e0cdb8a5452f71f1e3058026bdcbeea57c14ba7e
Desmond Rawls <captaingrover@gmail.com>
2013-11-04 18:51:18 -0500

    swap write_to_chat back to without .js

--- app/models/write_to_chat.rb
+++ app/models/write_to_chat.rb
@@ -1,8 +1,11 @@
 class WriteToChat
   def self.push_message(content, heroine, outgoing)
-    uri = URI.parse("http://162.243.70.202:9292/faye.js")
+    uri = URI.parse("http://162.243.70.202:9292/faye")
     data = {:content => content, :heroine => heroine, :outgoing => outgoing}
     message = {:channel => "/receive", :data => data}
     Net::HTTP.post_form(uri, :message => message.to_json)
   end
 end
+
+
+
